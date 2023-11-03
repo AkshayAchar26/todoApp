abab1188857d86bfd4a0594f3d69a2cc3bf7e2d2
akshay <akshaymanjunath26@gmail.com>
2023-11-03 13:22:38 +0530

    fixed errors and finished todo App

--- todo/src/contexts/TodoContext.js
+++ todo/src/contexts/TodoContext.js
@@ -1,17 +1,17 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-    todos: [
-        {
-            id:1,
-            todo:"My 1st Todo"
-            completed:false
-        } 
-    ],
-    addTodo = (todo) => {},
-    deleteTodo = (id) => {},
-    updateTodo = (id,todo) => {},
-    toggleComplete = (id) => {}
+  todos: [
+    {
+      id: 1,
+      todo: "My 1st Todo",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  deleteTodo: (id) => {},
+  updateTodo: (id, todo) => {},
+  toggleComplete: (id) => {},
 });
 
 export const TodoProvider = TodoContext.Provider;
